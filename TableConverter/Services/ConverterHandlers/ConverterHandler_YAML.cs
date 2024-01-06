845e14008af93ae8892701bf0dbefb2863e329d0
MrT-Stephens <92452307+MrT-Stephens@users.noreply.github.com>
2024-01-06 22:08:20 +0000

    Got YAML input working correctly

--- TableConverter/Services/ConverterHandlers/ConverterHandler_YAML.cs
+++ TableConverter/Services/ConverterHandlers/ConverterHandler_YAML.cs
@@ -3,8 +3,10 @@ using Avalonia.Platform.Storage;
 using Avalonia.Threading;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Data;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace TableConverter.Services.ConverterHandlers
@@ -59,7 +61,44 @@ namespace TableConverter.Services.ConverterHandlers
                 {
                     using var reader = new StreamReader(await input.OpenReadAsync());
 
-                    string data = (await reader.ReadToEndAsync()).Replace("---", string.Empty);
+                    Dictionary<string, string> yaml_data = null;
+
+                    bool first_line = true;
+                    while (!reader.EndOfStream)
+                    {
+                        string line = await reader.ReadLineAsync();
+
+                        if (line.StartsWith("---"))
+                        {
+                            continue;
+                        }
+                        if (line.StartsWith('-') && line.EndsWith('-'))
+                        {
+                            if (yaml_data != null)
+                            {
+                                if (first_line)
+                                {
+                                    column_values = yaml_data.Select(values => values.Key).ToList();
+                                    first_line = false;
+                                }
+
+                                row_values.Add(yaml_data.Select(values => values.Value).ToArray());
+                            }
+
+                            yaml_data = new Dictionary<string, string>();
+                        }
+                        else
+                        {
+                            string[] line_data = line.Split(':');
+
+                            if (line_data.Length == 2)
+                            {
+                                yaml_data.Add(line_data[0].Trim(), line_data[1].Trim());
+                            }
+                        }
+                    }
+
+                    row_values.Add(yaml_data.Select(values => values.Value).ToArray());
                 }
                 catch (Exception)
                 {
