9a76b163f9bf67855e9a305c4e94f1f4da83b680
MrT-Stephens <92452307+MrT-Stephens@users.noreply.github.com>
2024-01-09 21:15:22 +0000

    Fixed some issues, ready for packaging windows/android.

--- TableConverter.Android/MainActivity.cs
+++ TableConverter.Android/MainActivity.cs
@@ -1,15 +1,16 @@
 ﻿using Android.App;
 using Android.Content.PM;
-
 using Avalonia;
 using Avalonia.Android;
 
 namespace TableConverter.Android;
 
 [Activity(
-    Label = "TableConverter.Android",
-    Theme = "@style/MyTheme.NoActionBar",
+    Label = "TableConverter",
+    Name = "MrT_Stephens.TableConverter",
     Icon = "@drawable/icon",
+    Theme = "@style/MyTheme.NoActionBar",
+    AlwaysRetainTaskState = true,
     MainLauncher = true,
     ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize | ConfigChanges.UiMode)]
 public class MainActivity : AvaloniaMainActivity<App>
