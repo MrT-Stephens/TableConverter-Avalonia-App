d901835423cb937850fc74005220cfbc50033735
MrT-Stephens <92452307+MrT-Stephens@users.noreply.github.com>
2024-04-16 15:39:40 +0100

    Improved the data generation page

---
+++ TableConverter/DataModels/DataGenerationField.cs
@@ -0,0 +1,16 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace TableConverter.DataModels
+{
+    public partial class DataGenerationField : ObservableObject
+    {
+        [ObservableProperty]
+        private string _Name = string.Empty;
+
+        [ObservableProperty]
+        private string _Type = string.Empty;
+
+        [ObservableProperty]
+        private int _BlankPercentage;
+    }
+}
--- TableConverter/ViewModels/DataGenerationViewModel.cs
+++ TableConverter/ViewModels/DataGenerationViewModel.cs
@@ -1,4 +1,7 @@
-﻿using CommunityToolkit.Mvvm.Input;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+using TableConverter.DataModels;
 
 namespace TableConverter.ViewModels;
 
@@ -6,7 +9,21 @@ public partial class DataGenerationViewModel : ViewModelBase
 {
     #region Properties
 
+    [ObservableProperty]
+    private ObservableCollection<DataGenerationField> _DataGenerationFields = new();
+
+    [ObservableProperty]
+    private int _NumberOfRows;
+
     #endregion
+
+    public DataGenerationViewModel()
+    {
+        DataGenerationFields.Add(new DataGenerationField());
+        DataGenerationFields.Add(new DataGenerationField());
+        DataGenerationFields.Add(new DataGenerationField());
+        DataGenerationFields.Add(new DataGenerationField());
+    }
 
     #region Commands
 
