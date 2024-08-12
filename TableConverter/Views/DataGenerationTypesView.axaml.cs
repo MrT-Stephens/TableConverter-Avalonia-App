f70e7449674d0ebd0154293dc23fb3b4cb52db14
MrT-Stephens <92452307+MrT-Stephens@users.noreply.github.com>
2024-08-12 17:30:33 +0100

    Made the data generation types message box view addaptive to the screen size.

--- TableConverter/Views/DataGenerationTypesView.axaml.cs
+++ TableConverter/Views/DataGenerationTypesView.axaml.cs
@@ -1,4 +1,7 @@
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Data;
+using Avalonia.VisualTree;
 using System;
 using System.Collections.Generic;
 using TableConverter.DataGeneration.DataModels;
@@ -33,5 +36,17 @@ public partial class DataGenerationTypesView : UserControl
         DataContext = new DataGenerationTypesViewModel();
 
         InitializeComponent(true);
+
+        this.AttachedToVisualTree += (sender, args) =>
+        {
+            if (this.GetVisualRoot() is Window parent)
+            {
+                parent.GetObservable(TopLevel.ClientSizeProperty).Subscribe(size =>
+                {
+                    Width = size.Width - 40;
+                    Height = size.Height - 75;
+                });
+            }
+        };
     }
 }
