b9be1d31eff43f51f95636c2566d8b53e658833e
MrT-Stephens <92452307+MrT-Stephens@users.noreply.github.com>
2024-08-29 17:26:29 +0100

    Added the data generation types to the welcome page just like the converter types.

--- TableConverter/ViewModels/WelcomePageViewModel.cs
+++ TableConverter/ViewModels/WelcomePageViewModel.cs
@@ -1,6 +1,10 @@
 ﻿using Avalonia;
 using SukiUI.Dialogs;
 using SukiUI.Toasts;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using TableConverter.DataGeneration.DataModels;
 using TableConverter.Services;
 
 namespace TableConverter.ViewModels;
@@ -13,12 +17,31 @@ public partial class WelcomePageViewModel : BasePageViewModel
 
     #endregion
 
+    #region Properties 
+
+    public IReadOnlyDictionary<string, IReadOnlyList<DataGenerationType>> DataGenerationTypes { get; }
+
+    #endregion
+
     #region Constructors
 
-    public WelcomePageViewModel(ConverterTypesService converterTypes, ISukiDialogManager dialogManager, ISukiToastManager toastManager) 
+    public WelcomePageViewModel(ConverterTypesService converterTypes, DataGenerationTypesService dataGenerationTypes, ISukiDialogManager dialogManager, ISukiToastManager toastManager) 
         : base(dialogManager, toastManager, "Welcome", Application.Current?.Resources["HandWaveIcon"])
     {
         ConverterTypes = converterTypes;
+
+        DataGenerationTypes = dataGenerationTypes.Types
+            .Select(val => new KeyValuePair<string, IReadOnlyList<DataGenerationType>>(
+                val.Category,
+                new List<DataGenerationType>(
+                    dataGenerationTypes.Types
+                        .Where(types => types.Category == val.Category)
+                        .OrderBy(val => val.Name)
+                )
+            ))
+            .DistinctBy(kv => kv.Key)
+            .OrderBy(kv => kv.Key)
+            .ToDictionary(val => val.Key, val => val.Value);
     }
 
     #endregion
