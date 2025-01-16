47d632aabb18a9df6c048e1e361642c787c760cf
mrt-stephens <mrtstephens123@gmail.com>
2025-01-16 11:35:44 +0000

    Updated the background in the application.

--- TableConverter.DataGeneration/Interfaces/IModule.cs
+++ TableConverter.DataGeneration/Interfaces/IModule.cs
@@ -1,12 +1,12 @@
 namespace TableConverter.DataGeneration.Interfaces;
 
 /// <summary>
-///     Represents a module with a name, typically used to define components or sections of a larger system.
+/// Represents a module with a name, typically used to define components or sections of a larger system.
 /// </summary>
 public interface IModule
 {
     /// <summary>
-    ///     Gets the name of the module.
+    /// Gets the name of the module.
     /// </summary>
     public string ModuleName { get; }
 }
