7d359dc40fd552b34d2fe1c9fedaa118ba176f0f
mrt-stephens <mrtstephens123@gmail.com>
2025-03-23 11:30:03 +0000

    Add validation for incorrect PHP input file and implement test case for PHP input.

--- TableConverter.FileConverters.Tests/InputConverterTests/PhpInputTest.cs
+++ TableConverter.FileConverters.Tests/InputConverterTests/PhpInputTest.cs
@@ -1,3 +1,4 @@
+using TableConverter.FileConverters.ConverterHandlers;
 using TableConverter.FileConverters.ConverterHandlersOptions;
 using TableConverter.FileConverters.DataModels;
 using TableConverter.FileConverters.Tests.TestBase;
@@ -26,6 +27,19 @@ public class PhpInputTestCases : InputConverterTestCasesBase
     /// </summary>
     protected override IReadOnlyList<(string FileName, ConverterHandlerBaseOptions Options)> FailTestCases { get; } =
     [
-        
+        // Test case 1: Test with an incorrect PHP input file. Incorrect number of columns.
+        (
+            "test_input_incorrect_php_1.txt",   
+            new ConverterHandlerBaseOptions()
+        )
     ];
 }
+
+// ReSharper disable once UnusedType.Global
+/// <summary>
+///     Test class for <see cref="ConverterHandlerPhpInput" />
+///     Uses <see cref="PhpInputTestCases" /> as test cases.
+///     The <see cref="InputConverterTestBase{TInputConverter,TInputConverterData}" /> is used to instantiate the test
+///     cases for an input converter.
+/// </summary>
+public class PhpInputTest : InputConverterTestBase<ConverterHandlerPhpInput, PhpInputTestCases>;
