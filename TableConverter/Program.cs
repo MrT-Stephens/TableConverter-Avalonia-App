6629ba71447767d111df940eb5a3892be9740a96
mrt-stephens <mrtstephens123@gmail.com>
2025-04-02 18:58:19 +0100

    Add main program entry point and update project configuration for desktop application.

---
+++ TableConverter/Program.cs
@@ -0,0 +1,29 @@
+using System;
+using Avalonia;
+using Avalonia.Dialogs;
+
+namespace TableConverter.Desktop;
+
+internal class Program
+{
+    // Initialization code. Don't use any Avalonia, third-party APIs or any
+    // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+    // yet and stuff might break.
+    [STAThread]
+    public static void Main(string[] args)
+    {
+        BuildAvaloniaApp()
+            .StartWithClassicDesktopLifetime(args);
+    }
+
+    // Avalonia configuration, don't remove; also used by visual designer.
+    public static AppBuilder BuildAvaloniaApp()
+    {
+        var app = AppBuilder.Configure<App>()
+            .UsePlatformDetect()
+            .WithInterFont()
+            .LogToTrace();
+        
+        return app;
+    }
+}
