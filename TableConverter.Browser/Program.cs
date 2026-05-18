3fedfa3f870f8a85fb6a97271ecbd1bb3d38db5a
mrtstephens <mrtstephens123@gmail.com>
2026-05-18 12:04:57 +0100

    Add initial support for running the application in WebAssembly

---
+++ TableConverter.Browser/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using Avalonia;
+using Avalonia.Browser;
+
+namespace TableConverter.Browser;
+
+internal static class Program
+{
+    // Initialization code. Don't use any Avalonia, third-party APIs or any
+    // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+    // yet and stuff might break.
+    public static Task Main(string[] args)
+    {
+        return BuildAvaloniaApp()
+            .StartBrowserAppAsync("out");
+    }
+
+    // Avalonia configuration, don't remove; also used by visual designer.
+    private static AppBuilder BuildAvaloniaApp()
+    {
+        var app = AppBuilder.Configure<App>()
+            .WithInterFont();
+        
+        return app;
+    }
+}
