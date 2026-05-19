338d1137ec3e403739b1628f21e08cd2f0a80c77
mrtstephens <mrtstephens123@gmail.com>
2026-05-19 12:14:14 +0100

    Add MainContentView user control for the application

---
+++ TableConverter/Views/MainContentView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace TableConverter.Views;
+
+public partial class MainContentView : UserControl
+{
+    public MainContentView()
+    {
+        InitializeComponent();
+    }
+}
+
